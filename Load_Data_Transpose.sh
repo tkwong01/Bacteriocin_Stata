a38d57b6b0c051fa666331f8ba14eed127b0f49c
tkwong01 <tkwong01@tufts.edu>
2026-04-07 19:32:12 -0400

    Update Load_Data_Transpose.sh

--- Load_Data_Transpose.sh
+++ Load_Data_Transpose.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+#SBATCH --job-name=STATA_MP
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8   # Requesting 8 cores for MP
+#SBATCH --mem=60G
 
+module load stata/18
+stata-mp -b do your_script.do
